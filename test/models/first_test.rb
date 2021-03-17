2faa4a5cd4334591f081357f33f51a2caa9d55ed
levis <leviprudenciont@gmail.com>
2021-03-17 11:14:07 -0300

    feat: add User model

---
+++ test/models/first_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class  FirstTest  < ActiveSupport:: TestCase
+    test "Primeiro test" do
+        varivel = "Levi"
+        assert_equal "Levi", varivel 
+    end
+
+end
---
+++ test/system/signups_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class SignupsTest < ApplicationSystemTestCase
+  test "User register with the correct values" do
+    
+    visit "/"
+    
+    click_on "Cadastre-se"
+    
+    fill_in "Nome",   with: "Anonimo"
+    fill_in "E-mail",   with: "anonimo@mail.com"
+    fill_in "Senha",  with: "123456"
+    fill_in "Confirme a senha",   with: "123456"
+
+    click_on "Cadastrar"
+
+    assert_text "Cadastro realizado com sucesso!!!"
+  
+  end
+end
