318108c2d73958245c4d1c14fe86271fe5b91b0e
levis <leviprudenciont@gmail.com>
2021-03-21 17:41:51 -0300

    refactor: move LoginTest  to a helper

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -6,29 +6,13 @@ class LoginsTest < ApplicationSystemTestCase
   end
 
   test "User login with the correct values" do
-
-    visit root_path
-    click_link "Entrar"
-    
-    
-    fill_in "E-mail",   with: @user.email
-    fill_in "Senha",  with: "password"
-    
-    click_button "Entrar"
-    
+    login_as(@user)
     assert_text "Bem vindo Anonimo!!!"
   end
   
   test "User login with the invalid values" do
     
-    visit root_path  
-    click_link "Entrar"
-    
-    
-    fill_in "E-mail",   with: @user.email
-    fill_in "Senha",  with: "123456"
-    
-    click_button "Entrar"
+    login_as(@user, "123456")
     
     assert_equal new_session_path, current_path
     assert_text "E-mail ou Senha inválidos!!!"
