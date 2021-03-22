7be313ddb42c21d736d6a308d6a652e2661347c6
levis <leviprudenciont@gmail.com>
2021-03-22 15:25:10 -0300

    feat: ensure call logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create 
-     user = User.find_by(email: params[:user][:email])
+        user = User.find_by(email: params[:user][:email])
 
-     if user && user.authenticate(params[:user][:password])
+        if user && user.authenticate(params[:user][:password])
+            reset_session
+            session[:user_id] = user.id
+            redirect_to root_path, notice: t(".welcome", name: user.name)
+        else
+            redirect_to new_session_path, alert: t(".invalid_credentials") 
+        end    
+    end
+
+    def destroy
         reset_session
-        session[:user_id] = user.id
-        redirect_to root_path, notice: t(".welcome", name: user.name)
-    else
-        redirect_to new_session_path, alert: t(".invalid_credentials")
-
-     end
-        
+        redirect_to new_session_path, notice: t(".session_finished")
     end
     
 end
